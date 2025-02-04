4a15d0ea7336558602ae0486ff54980f58037d01
Senior Ion <curmeiion3@gmail.com>
2025-02-04 17:53:54 +0200

    Updated commit lint error

--- homework/Curmei_Ion/homework/lesson3/task2.py
+++ homework/Curmei_Ion/homework/lesson3/task2.py
@@ -1,4 +1,4 @@
 x = 10
 y = 5
 
-print((x - y) / (1 + x*y))
+print((x - y) / (1 + x * y))
--- homework/Curmei_Ion/homework/lesson3/task3.py
+++ homework/Curmei_Ion/homework/lesson3/task3.py
@@ -1,6 +1,6 @@
 c = 6
 d = 9
 
-print((c + d ) / 2 )
+print((c + d ) / 2)
 
-print((c * d ) ** 0.5 )
+print((c * d ) ** 0.5)
