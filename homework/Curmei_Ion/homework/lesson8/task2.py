faefed19317641370e63f992bc9c2251df207ae3
Senior Ion <curmeiion3@gmail.com>
2025-02-17 23:04:27 +0200

    uptade task2.py fibonacci generator

--- homework/Curmei_Ion/homework/lesson8/task2.py
+++ homework/Curmei_Ion/homework/lesson8/task2.py
@@ -1,13 +1,20 @@
 def fibonacci(n):
     a, b = 0, 1
-    for i in range(n):
+    while True:
         yield a
         a, b = b, a + b
 
 
-for i in 5, 200, 1000, 2000:
-    fib_gen = fibonacci(i)
-    fib_number = 0
-    for num in fib_gen:
-        fib_number = num
-    print(f'Fibonaci({i}) = {fib_number}')
+count = 0
+
+for num in fibonacci(10000000000000000000000000000000):
+    if count == 5:
+        print(num)
+    elif count == 200:
+        print(num)
+    elif count == 1000:
+        print(num)
+    elif count == 100000:
+        print(num)
+        break
+    count += 1
