515e8aceecef65a3727f8fd06809555c02ed55de
Ion <curmeiion3@gmail.com>
2025-03-22 23:07:47 +0200

    modify Script.sql

--- homework/Curmei_Ion/homework/lesson14/Script.sql
+++ homework/Curmei_Ion/homework/lesson14/Script.sql
@@ -1,25 +1,25 @@
-insert into students (id,name,second_name,group_id) values (20000,'student','test',50000);
-insert into books (id,title,taken_by_student_id) values (5,'Mathematics',50000);
-insert into books (id,title,taken_by_student_id) values (6,'Economy',50000);
-insert into groups(id,title,start_date,end_date) values (50000,'Automation Testers','Feb 2020','June 2020');
+insert into students (id,name,second_name) values (20000,'student','test');
+insert into books (title,taken_by_student_id) values ('Mathematics',20000);
+insert into books (title,taken_by_student_id) values ('Economy',20000);
+insert into groups(title,start_date,end_date) values ('Automation Testers','Feb 2020','June 2020');
 insert into subjets (id,title) values
 (10001,'operation basic'),
 (10002,'economy');
 INSERT INTO lessons (title,subject_id) VALUES 
 ('math base',10001),
 ('fundamental economy',10002);
-insert into marks (value,lesson_id,student_id) values (8,10001,50000),(10,10002,50000);
+insert into marks (value,lesson_id,student_id) values (8,10001,50000),(10,10002,20000);
 
 
 SELECT m.value AS mark, l.title AS lesson_title, s.title AS subject_title
 FROM marks m
 JOIN lessons l ON m.lesson_id = l.id
 JOIN subjets s ON l.subject_id = s.id
-WHERE m.student_id = 50000;
+WHERE m.student_id = 20000;
 
 SELECT b.title AS book_title
 FROM books b
-WHERE b.taken_by_student_id = 50000;
+WHERE b.taken_by_student_id = 20000;
 
 SELECT 
     st.name AS student_name,
@@ -37,4 +37,4 @@ LEFT JOIN books b ON st.id = b.taken_by_student_id
 LEFT JOIN marks m ON st.id = m.student_id
 LEFT JOIN lessons l ON m.lesson_id = l.id
 LEFT JOIN subjets s ON l.subject_id = s.id
-WHERE st.id = 50000;
+WHERE st.id = 20000;
