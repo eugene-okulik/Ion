91676881bf7a4e64b8dbe1e4067d0658bb3d77de
Curmei Ion <curmeiion3@gmail.com>
2025-07-02 15:13:48 +0300

    resolve lint error

--- homework/Curmei_Ion/homework/test_api_icurmei/conftest.py
+++ homework/Curmei_Ion/homework/test_api_icurmei/conftest.py
@@ -1,15 +1,18 @@
-import pytest
+import pytest  # coment for lint
 from endpoints.create_post import CreatePost
 from endpoints.update_post import UpdatePost
 from endpoints.delete_post import DeletePost
+
 
 @pytest.fixture()
 def create_post_endpoint():
     return CreatePost()
 
+
 @pytest.fixture()
 def update_post_endpoint():
     return UpdatePost()
+
 
 @pytest.fixture()
 def delete_post_endpoint():
